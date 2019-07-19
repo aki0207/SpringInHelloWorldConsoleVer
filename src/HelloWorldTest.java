ac4da86b8404aa8585acafe48956fcf804ff9144
aki0207 <forever.look.aki@docome.ne.jp>
2019-07-19 16:28:51 +0900

    spring完全に理解した

---
+++ src/HelloWorldTest.java
@@ -0,0 +1,11 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class HelloWorldTest {
+	public static void main(String[] args) {
+		ApplicationContext context = new FileSystemXmlApplicationContext("SpringTest.xml");
+
+		SpringBean bean = (SpringBean) context.getBean("helloWorld");
+		bean.show();
+	}
+}
---
+++ src/SpringBean.java
@@ -0,0 +1,15 @@
+public class SpringBean {
+	private String text;
+
+	public void show() {
+		System.out.println(text);
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+}
